631db0c1fbbb982e8f713873b42bfa036e77ff51
Ramesh Pradhan <pyrameshpradhan@gmail.com>
2024-08-09 18:13:01 +0545

    draft detail

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -3,4 +3,8 @@ from blog_app import views
 
 urlpatterns = [
     path("", views.post_list, name="post-list"),
+    path("post-detail/<int:pk>/", views.post_detail, name="post-detail"),
+    path("post-delete/<int:pk>/", views.post_delete, name="post-delete"),
+    path("draft-list/", views.draft_list, name="draft-list"),
+    path("draft-detail/<int:pk>/", views.draft_detail, name="draft-detail"),
 ]
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,12 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
 
 from blog_app.models import Post
 
 
 def post_list(request):
-    posts = Post.objects.all()
+    posts = Post.objects.filter(published_at__isnull=False)
     return render(
         request,
         "post_list.html",
         {"posts": posts},
+    )
+
+
+def post_detail(request, pk):
+    post = Post.objects.get(pk=pk, published_at__isnull=False)
+    return render(
+        request,
+        "post_detail.html",
+        {"post": post},
+    )
+
+
+@login_required
+def post_delete(request, pk):
+    post = Post.objects.get(pk=pk)
+    post.delete()
+    return redirect("post-list")
+
+
+@login_required
+def draft_list(request):
+    posts = Post.objects.filter(published_at__isnull=True)
+    return render(
+        request,
+        "draft_list.html",
+        {"posts": posts},
+    )
+
+
+@login_required
+def draft_detail(request, pk):
+    post = Post.objects.get(pk=pk, published_at__isnull=True)
+    return render(
+        request,
+        "draft_detail.html",
+        {"post": post},
     )
