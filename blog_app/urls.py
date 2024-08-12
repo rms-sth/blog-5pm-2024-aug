48eccf42520b17ff29b4b9b710f7f3aabbc46ed7
Ramesh Pradhan <pyrameshpradhan@gmail.com>
2024-08-12 17:35:11 +0545

    Finish function based views

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path("post-delete/<int:pk>/", views.post_delete, name="post-delete"),
     path("draft-list/", views.draft_list, name="draft-list"),
     path("draft-detail/<int:pk>/", views.draft_detail, name="draft-detail"),
+    path("post-create/", views.post_create, name="post-create"),
+    path("draft-publish/<int:pk>/", views.draft_publish, name="draft-publish"),
+    path("post-update/<int:pk>/", views.post_update, name="post-update"),
 ]
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
+from django.utils import timezone
 
+from blog_app.forms import PostForm
 from blog_app.models import Post
 
 
@@ -46,4 +48,50 @@ def draft_detail(request, pk):
         request,
         "draft_detail.html",
         {"post": post},
+    )
+
+
+@login_required
+def post_create(request):
+    form = PostForm()
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user  # logged in user will be the author
+            post.save()
+            return redirect("draft-detail", pk=post.pk)
+
+    return render(
+        request,
+        "post_create.html",
+        {"form": form},
+    )
+
+
+@login_required
+def draft_publish(request, pk):
+    post = Post.objects.get(pk=pk, published_at__isnull=True)
+    post.published_at = timezone.now()
+    post.save()
+    return redirect("post-list")
+
+
+@login_required
+def post_update(request, pk):
+    post = Post.objects.get(pk=pk)
+    form = PostForm(instance=post)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save()
+            if post.published_at:
+                return redirect("post-detail", post.pk)
+            else:
+                return redirect("draft-detail", post.pk)
+
+    return render(
+        request,
+        "post_create.html",
+        {"form": form},
     )
